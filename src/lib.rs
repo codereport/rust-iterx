c7bfbb358484a8713199f8b1b54eb58eecdfac9f
Conor Hoekstra <codereport@outlook.com>
2022-11-09 21:59:36 -0500

    option_lift

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
-// #[cfg(test)]
-// use std::ops::Add;
+#[cfg(test)]
+use std::ops::Add;
 
-// pub fn option_lift<F, I, O>(f: F) -> impl Fn(I, I) -> Option<O>
-// where
-//     F: Fn(I, I) -> O,
-// {
-//     return |*acc, x| {
-//         *acc = f(*acc, x);
-//         Some(*acc)
-//     };
-// }
+pub fn option_lift<T: Copy>(f: &dyn Fn(&mut T, T) -> T) -> impl Fn(&mut T, T) -> Option<T> + '_ {
+    |acc, x| {
+        *acc = f(&mut *acc, x);
+        Some(*acc)
+    }
+}
+
+#[cfg(test)]
+fn w<T: Copy, O>(binop: &dyn Fn(T, T) -> O) -> impl Fn(T) -> O + '_ {
+    |x| binop(x, x)
+}
 
 // pub trait Iterscans : Iterator {
 //     fn scan_while(self, f: F) {}
@@ -20,7 +22,7 @@
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
     #[test]
     fn test_scan_not_what_i_want() {
@@ -29,13 +31,14 @@ mod tests {
         assert_eq!(
             input
                 .into_iter()
-                // .scan(0, &option_lift(&Add::add))
-                .scan(0, |acc, x| {
-                    *acc += x;
-                    Some(*acc)
-                })
+                .scan(0, &option_lift(&|a, b| *a + b))
                 .collect::<Vec<_>>(),
             expected
         );
+    }
+
+    #[test]
+    fn test_w_combinator() {
+        assert_eq!(w(&Add::add)(1), 2);
     }
 }
