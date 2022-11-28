0d5fef6da60fc4e0e051e09a266e089fc6d32004
Conor Hoekstra <codereport@outlook.com>
2022-11-28 12:49:17 -0500

    prescan + some renaming

--- src/lib.rs
+++ src/lib.rs
@@ -25,12 +25,12 @@ fn w<T: Copy, O>(binop: &dyn Fn(T, T) -> O) -> impl Fn(T) -> O + '_ {
 pub trait Iterscans: Iterator {
     // Name scan_ so as to not collide with std::iter::Iterator::scan
     // std::iter::Iterator::scan should really be scan_while
-    fn scan_<F>(self, f: F) -> Prescan<Self, Self::Item, F>
+    fn scan_<F>(self, f: F) -> Scan_<Self, Self::Item, F>
     where
         Self: Sized,
         F: FnMut(&Self::Item, &Self::Item) -> Self::Item,
     {
-        Prescan::new(self, f)
+        Scan_::new(self, f)
     }
 
     // A renaming of https://doc.rust-lang.org/src/core/iter/traits/iterator.rs.html#1420
@@ -41,19 +41,66 @@ pub trait Iterscans: Iterator {
     {
         self.scan(initial_state, f)
     }
+
+    fn prescan<St, F>(self, initial_state: St, f: F) -> Prescan<Self, St, F>
+    where
+        Self: Sized,
+        F: FnMut(&St, &Self::Item) -> St,
+    {
+        Prescan::new(self, initial_state, f)
+    }
 }
 
 impl<T: ?Sized> Iterscans for T where T: Iterator {}
 
 #[derive(Clone)]
-pub struct Prescan<I, T, F> {
+pub struct Prescan<I, St, F> {
+    iter: I,
+    f: F,
+    state: Option<St>,
+}
+
+impl<I, St, F> Prescan<I, St, F> {
+    fn new(iter: I, initial_state: St, f: F) -> Self {
+        Self {
+            iter,
+            f,
+            state: Some(initial_state),
+        }
+    }
+}
+
+impl<I, St, F> Iterator for Prescan<I, St, F>
+where
+    I: Iterator,
+    F: FnMut(&St, &I::Item) -> St,
+{
+    type Item = St;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let state = self.state.take()?;
+
+        if let Some(x) = self.iter.next() {
+            self.state = Some((self.f)(&state, &x));
+        }
+
+        Some(state)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let (lb, ub) = self.iter.size_hint();
+        (lb + 1, ub.map(|ub| ub + 1))
+    }
+}
+#[derive(Clone)]
+pub struct Scan_<I, T, F> {
     iter: I,
     f: F,
     state: Option<T>,
     is_first: bool,
 }
 
-impl<I, T, F> Prescan<I, T, F>
+impl<I, T, F> Scan_<I, T, F>
 where
     I: Iterator<Item = T>,
 {
@@ -67,7 +114,7 @@ where
     }
 }
 
-impl<I, T, F> Iterator for Prescan<I, T, F>
+impl<I, T, F> Iterator for Scan_<I, T, F>
 where
     I: Iterator<Item = T>,
     F: FnMut(&T, &T) -> T,
@@ -116,8 +163,14 @@ mod tests {
     }
 
     #[test]
-    fn test_scanl1() {
+    fn test_scan_() {
         assert_equal(vec![1, 1, 1].into_iter().scan_(|x, y| x + y), 1..=3);
         assert_equal((1..=5).scan_(|x, y| x + y), vec![1, 3, 6, 10, 15]);
+    }
+
+    #[test]
+    fn test_prescan() {
+        assert_equal(vec![1, 1, 1].into_iter().prescan(0, |x, y| x + y), 0..=3);
+        assert_equal((1..=5).prescan(0, |x, y| x + y), vec![0, 1, 3, 6, 10, 15]);
     }
 }
